36804e7c9a5bbcedba5037d6968cec0fbe8622e3
Tayler Skirvin <49698401+tds325@users.noreply.github.com>
2021-05-16 13:50:39 -0700

    Readability
    Code was getting messy, so restructured to be more readable

--- screen.py
+++ screen.py
@@ -8,74 +8,101 @@ os.chdir("..")
 
 class tkinterGUI:
 
-    # window is the parent widget
+    # window class is the parent (root) widget for tkinter library
     def __init__(self, window):
-        # golden ratio
+        # golden ratio magic number for reference
         ratio = 1.618
 
+        # magic number TODO - make window resizable
         self.canvas_width = 970
         self.canvas_height = round(self.canvas_width / ratio)
+        self.wavecanvas_width = self.canvas_width / 2
+        self.wavecanvas_height = self.canvas_height / 3
 
+        print(self.__dict__)
+
+        self.initialize_canvases(window)
+
+        self.initialize_bitmap(window)
+
+
+    def initialize_canvases(self, window):
+        # gui window properties
         self.window = window
         window.title("Waves")
         window.geometry(f"{self.canvas_width}x{self.canvas_height}+10+10")
 
+        # black background window
         self.wavecanvas = tkinter.Canvas(window, bg="black", \
         width= self.canvas_width, height = self.canvas_height, highlightbackground = "black")
         self.wavecanvas.place(relx = "0", rely = "0")
 
-        wavecanvas_width = self.canvas_width / 2
-        wavecanvas_height = self.canvas_height / 3
-        print(wavecanvas_width)
-        print(wavecanvas_height)
-
+        # smaller window
         self.canvas = tkinter.Canvas(window, bg="LightSteelBlue3", \
-        width=wavecanvas_width,height = wavecanvas_height)
-
+        width=self.wavecanvas_width,height = self.wavecanvas_height)
         #dividing to balance second canvas in the middle
         x_ratio = ((self.canvas_width) / self.canvas_width)/4
         y_ratio = ((self.canvas_height) / self.canvas_height)/6
-
         self.canvas.place(relx = f"{x_ratio}", x = "0", rely = f"{y_ratio}")
 
+
+    def initialize_bitmap(self, window):
+        # 'overwrite_bitmap' returns the file name of the bitmap
         filename = overwrite_bitmap(self.canvas_width, self.canvas_height)
+
+        # filename of bitmap found using os - Root directory -> xbm file
         bitmap = os.path.join(ROOT_DIR, filename)
 
         print(f"bitmap: {bitmap}")
 
-        self.bitmap = self.canvas.create_bitmap("0", "0", anchor = "nw", \
+        # bitmap created in tkinter using its 'create_bitmap' method
+        self.bitmap = self.canvas.create_bitmap("0", "0", anchor = "w", \
         bitmap = f"@{bitmap}", background = "green")
 
+# after writing initial bitmap boilerplate, loops through and writes each
+# '0xXX' term to be displayed in the file - returns the string to be written
+# in the bitmap file in object
 def overwrite_bitmap(width, height):
-    print(f"{width}\n{height}")
+    # print(f"{width}\n{height}")
     filename = "bitmap.xbm"
     file = open(f"{ROOT_DIR}\{filename}", "w")
+
     file.write(f'#define im_width {width}\n#define im_height {height}\n\
 static char im_bits[] = {{\n')
+
     ans = write_bitmap_lines(width, height)
     file.write(f"{ans}}};")
+
     file.close()
 
     return filename
 
+# given the width and height of the bitmap, return a string of the bitmap
+# values to be written in the xbm file
 def write_bitmap_lines(width, height):
+    width=485#488
+    height=200#304
+    print(f"\twidth: {width}\n\theight: {height}")
     file_write = ""
     count = 0
-    term = width * height / 4
+    innercount = 0
+    term = 73200
 
-    while(count < term - 1):
-        if((count) % width == 0):
-            file_write += "0x00, \n"
-        elif(count % 61 == 0):
-            file_write += "0x0c, "
-        #elif((count + 1) % 64 == 0):
-            #file_write += "0xff"
-        else:
-            file_write += "0x00, "
-        count += 1
+    test = "0x01, 0x03, 0x07, 0x0f, 0x1f, 0x3f, 0x7f, 0xff, "
+    for count in range(int(term / 8)):
+        for innercount in range(8):
 
-    file_write += "0x10 "
+            #if((count) % width == 0):
+            #    file_write += "0x00, \n"
+            #elif(count % 61 == 0):
+            #    file_write += "0x0c, "
+            #else:
+            #    file_write += "0x00, "
+            count += 1
+        file_write += test
 
+    file_write += "0x00 "
+    print(f"\tcount: {count}")
     return file_write
 
 
@@ -83,6 +110,7 @@ def main():
 
     root = tkinter.Tk()
     root.resizable(False, False)
+
     gui = tkinterGUI(root)
     root.mainloop()
 
