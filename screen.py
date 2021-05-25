c4f7b0f3b950aef43bf1fa1b9ca40cc7f8e18e8f
Tayler Skirvin <49698401+tds325@users.noreply.github.com>
2021-05-25 10:39:14 -0700

    Bitmap removal
    bit manipulation was proving inefficient and unweildy

--- screen.py
+++ screen.py
@@ -2,6 +2,7 @@
 
 import tkinter, tkinter.filedialog
 import os
+import matplotlib
 
 ROOT_DIR = os.path.dirname(os.path.abspath("screen.py"))
 os.chdir("..")
@@ -12,7 +13,6 @@ class tkinterGUI:
     def __init__(self, window):
         # golden ratio magic number for reference
         ratio = 1.618
-
         # magic number TODO - make window resizable
         self.canvas_width = 970
         self.canvas_height = round(self.canvas_width / ratio)
@@ -23,7 +23,7 @@ class tkinterGUI:
 
         self.initialize_canvases(window)
 
-        self.initialize_bitmap(window)
+        #self.initialize_bitmap(window)
 
 
     def initialize_canvases(self, window):
@@ -45,67 +45,6 @@ class tkinterGUI:
         y_ratio = ((self.canvas_height) / self.canvas_height)/6
         self.canvas.place(relx = f"{x_ratio}", x = "0", rely = f"{y_ratio}")
 
-
-    def initialize_bitmap(self, window):
-        # 'overwrite_bitmap' returns the file name of the bitmap
-        filename = overwrite_bitmap(self.canvas_width, self.canvas_height)
-
-        # filename of bitmap found using os - Root directory -> xbm file
-        bitmap = os.path.join(ROOT_DIR, filename)
-
-        print(f"bitmap: {bitmap}")
-
-        # bitmap created in tkinter using its 'create_bitmap' method
-        self.bitmap = self.canvas.create_bitmap("0", "0", anchor = "w", \
-        bitmap = f"@{bitmap}", background = "green")
-
-# after writing initial bitmap boilerplate, loops through and writes each
-# '0xXX' term to be displayed in the file - returns the string to be written
-# in the bitmap file in object
-def overwrite_bitmap(width, height):
-    # print(f"{width}\n{height}")
-    filename = "bitmap.xbm"
-    file = open(f"{ROOT_DIR}\{filename}", "w")
-
-    file.write(f'#define im_width {width}\n#define im_height {height}\n\
-static char im_bits[] = {{\n')
-
-    ans = write_bitmap_lines(width, height)
-    file.write(f"{ans}}};")
-
-    file.close()
-
-    return filename
-
-# given the width and height of the bitmap, return a string of the bitmap
-# values to be written in the xbm file
-def write_bitmap_lines(width, height):
-    width=485#488
-    height=200#304
-    print(f"\twidth: {width}\n\theight: {height}")
-    file_write = ""
-    count = 0
-    innercount = 0
-    term = 73200
-
-    test = "0x01, 0x03, 0x07, 0x0f, 0x1f, 0x3f, 0x7f, 0xff, "
-    for count in range(int(term / 8)):
-        for innercount in range(8):
-
-            #if((count) % width == 0):
-            #    file_write += "0x00, \n"
-            #elif(count % 61 == 0):
-            #    file_write += "0x0c, "
-            #else:
-            #    file_write += "0x00, "
-            count += 1
-        file_write += test
-
-    file_write += "0x00 "
-    print(f"\tcount: {count}")
-    return file_write
-
-
 def main():
 
     root = tkinter.Tk()
@@ -117,3 +56,64 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+    #def initialize_bitmap(self, window):
+        # 'overwrite_bitmap' returns the file name of the bitmap
+        #filename = overwrite_bitmap(self.canvas_width, self.canvas_height)
+
+        # filename of bitmap found using os - Root directory -> xbm file
+        #bitmap = os.path.join(ROOT_DIR, filename)
+
+        #print(f"bitmap: {bitmap}")
+
+        # bitmap created in tkinter using its 'create_bitmap' method
+        #self.bitmap = self.canvas.create_bitmap("0", "0", anchor = "w", \
+        #bitmap = f"@{bitmap}", background = "green")
+
+# after writing initial bitmap boilerplate, loops through and writes each
+# '0xXX' term to be displayed in the file - returns the string to be written
+# in the bitmap file in object
+#def overwrite_bitmap(width, height):
+    # print(f"{width}\n{height}")
+    #filename = "bitmap.xbm"
+    #file = open(f"{ROOT_DIR}\{filename}", "w")
+
+    #file.write(f'#define im_width {width}\n#define im_height {height}\n\
+#static char im_bits[] = {{\n')
+
+    #ans = write_bitmap_lines(width, height)
+    #file.write(f"{ans}}};")
+
+    #file.close()
+
+    #return filename
+
+# given the width and height of the bitmap, return a string of the bitmap
+# values to be written in the xbm file
+#def write_bitmap_lines(width, height):
+    #width=485
+    #height=200
+    #print(f"\twidth: {width}\n\theight: {height}")
+    #file_write = ""
+    #count = 0
+    #innercount = 0
+    #term = 73200
+
+    #test = "0x01, 0x03, 0x07, 0x0f, 0x1f, 0x3f, 0x7f, 0xff, "
+    #for count in range(int(term / 8) - 1):
+        #for innercount in range(8):
+            #count += 1
+        #file_write += test
+
+    #file_write += "0x00, "
+    #file_write += "0x00, "
+    #file_write += "0x00, "
+    #file_write += "0x00, "
+    #file_write += "0x00, "
+    #file_write += "0x00, "
+    #file_write += "0x00, "
+    #file_write += "0x00 "
+    #print(f"\tcount: {count}")
+    #return file_write
